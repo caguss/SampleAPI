cd876575828912c73ebd4759a31c16aab7b6dab0
coever.BCS <bcs@coever.co.kr>
2021-01-13 16:14:35 +0900

    초기 커밋.

---
+++ SampleAPI/Restful_Coever.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SampleAPI
+{
+    partial class Restful_Coever
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "스케쥴링 시작";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP : db3.coever.co.kr";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DB: Coever_Web";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Table : 업체사용이력 참고";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "1000";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(117, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 21);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "업체코드";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "프로젝트번호";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 41);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "스케쥴링 종료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3600000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(242, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(253, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "바꾸지마세요. 어차피 하이월드밖에 안됨 지금";
+            // 
+            // Restful_Coever
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 190);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Restful_Coever";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SampleAPI/Restful_Coever.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using MySql.Data.MySqlClient;
+
+namespace SampleAPI
+{
+    public partial class Restful_Coever : Form
+    {
+        private string strAPI = "/CompanyUseage"; // API 설정
+        private DataSet ds = new DataSet();
+        DateTime last_record;
+        public Restful_Coever()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+
+            last_record = DateTime.Now;
+            await SendMessage();
+
+            timer1.Start();
+        }
+
+        private async Task SendMessage()
+        {
+            try
+            {
+                string strConn = "Server=m.coever.co.kr;Database=coever_mes_hwt;Uid=dbmes;Pwd=dbmes1!;"; //"Server=112.186.117.140;Database=coever_mes_hwt;Uid=dbmes;Pwd=dbmes1!;";
+
+                using (MySqlConnection conn = new MySqlConnection(strConn))
+                {
+                    conn.Open();
+
+                    CheckRowData(conn);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("db 오류");
+            }
+
+            try
+            {
+                string url = "http://db2.coever.co.kr:21125/CompanyUseage";
+                var httpContent = new System.Net.Http.StringContent("");
+                HttpClient client = new System.Net.Http.HttpClient
+                {
+                    BaseAddress = new Uri(url)
+                };
+
+
+                //DATASET ds 수주,발주, 작지, 재고, 생산계획, 출하, 생산실적 순
+
+                //A10 수주
+
+                string data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                 "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                 "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                 "\"구분코드\":" + "\"" + "A10" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                 "\"로우갯수\":" + Convert.ToInt32(ds.Tables[0].Rows[0]["rowcount"].ToString()) + "," +
+                                 "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[0].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                 "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[0].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                 "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                               "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                var response1 = await client.PostAsync(url, httpContent);
+
+                //A20 발주
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A20" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[1].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[1].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[1].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+
+                //A30 작지
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A30" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[2].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[2].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[2].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+
+                //A40 재고
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A40" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[3].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[3].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[3].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+
+                //A50 재고
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A50" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[3].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[3].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[3].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+
+                //A60 출하
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A60" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[5].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[5].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[5].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+
+                //A25 생산계획
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A25" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[4].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[4].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[4].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+
+                //A26 생산실적
+
+                data = "[{ \"회사코드\":" + "\"" + textBox1.Text + "\"" + "," + // 코에버 공통 1000? 1051?
+                                "\"프로젝트번호\":" + Convert.ToInt32(textBox2.Text) + "," + // 프로젝트 번호  담당자 문의 or DB 확인 ( 업체 생성 PMS)
+                                "\"날짜\": " + "\"" + TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, "Korea Standard Time").ToString("yyyy-MM-dd") + "\"" + "," +
+                                "\"구분코드\":" + "\"" + "A26" + "\"" + "," +  // 공통 코드 문서 참고 ( A10 : 수주 등등)
+                                "\"로우갯수\":" + Convert.ToInt32(ds.Tables[6].Rows[0]["rowcount"].ToString()) + "," +
+                                "\"최근등록일자\":" + "\"" + ((DateTime)ds.Tables[6].Rows[0]["reg_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"최근수정일자\":" + "\"" + ((DateTime)ds.Tables[6].Rows[0]["up_date"]).ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "\"" + "," +
+                                "\"비밀번호\":" + "\"" + "CoeverSmartFactoryCompanyUseage" + "\"" +
+                              "}]";
+
+                httpContent = new System.Net.Http.StringContent(data, Encoding.UTF8, "application/json");
+
+                response1 = await client.PostAsync(url, httpContent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("방찬석 불러와 : " + ex.Message);
+            }
+        }
+
+        private DataSet CheckRowData(MySqlConnection conn)
+        {
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.CommandText = "CheckRowData_R10";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection = conn;
+            MySqlDataAdapter da = new MySqlDataAdapter();
+
+            da.SelectCommand = cmd;
+            da.Fill(ds);
+            return ds;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            button1.Enabled = true;
+            timer1.Stop();
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            if (last_record.Day != DateTime.Now.Day)
+            {
+                await SendMessage();
+            }
+        }
+    }
+}
